f11f4bb439ee88887f05ade370e10dfe57579087
Tushar Patil <tusharpatil3688@gmail.com>
2020-06-24 11:50:31 +0530

    fix:calculate additional salary today

--- overtime_calculation/overtime_calculation/additional_salary.py
+++ overtime_calculation/overtime_calculation/additional_salary.py
@@ -5,55 +5,55 @@ import datetime
 import dateutil.relativedelta
 
 def execute():
-	if datetime.date.today()==datetime.date.today().replace(day=21):
-		for emp in frappe.get_all('Employee'):
-			total_cost_ot_1=0
-			total_cost_ot_2=0
-			sc1_row=[]
-			sc2_row=[]
-			overtime_list1=[]
-			overtime_list2=[]
-			fltr={'employee':emp.name}
-			fltr.update({'date': ['between', [add_months(getdate(nowdate()),-1), add_days(getdate(nowdate()),-1)]]})
-			for ov in frappe.get_all('Overtime Calculation',filters={'employee':emp.name},fields=['name','date','salary_component','overtime_period','overtime_cost']):
-				if ov.get('salary_component')=='OT 1.5 Amt':
-					sc1_row.append({
-						'overtime_detail':ov.get('name'),
-						'date':ov.get('date'),
-						'overtime':ov.get('overtime_period')
-					})
-					total_cost_ot_1+=float(ov.get('overtime_cost'))
-					overtime_list1.append(ov.get('overtime_period'))
-				else:
-					sc2_row.append({
-						'overtime_detail':ov.get('name'),
-						'date':ov.get('date'),
-						'overtime':ov.get('overtime_period')
-					})
-					total_cost_ot_2+=float(ov.get('overtime_cost'))
-					overtime_list2.append(ov.get('overtime_period'))
+	# if datetime.date.today()==datetime.date.today().replace(day=21):
+	for emp in frappe.get_all('Employee'):
+		total_cost_ot_1=0
+		total_cost_ot_2=0
+		sc1_row=[]
+		sc2_row=[]
+		overtime_list1=[]
+		overtime_list2=[]
+		fltr={'employee':emp.name}
+		fltr.update({'date': ['between', [add_months(getdate(nowdate()),-1), add_days(getdate(nowdate()),-1)]]})
+		for ov in frappe.get_all('Overtime Calculation',filters={'employee':emp.name},fields=['name','date','salary_component','overtime_period','overtime_cost']):
+			if ov.get('salary_component')=='OT 1.5 Amt':
+				sc1_row.append({
+					'overtime_detail':ov.get('name'),
+					'date':ov.get('date'),
+					'overtime':ov.get('overtime_period')
+				})
+				total_cost_ot_1+=float(ov.get('overtime_cost'))
+				overtime_list1.append(ov.get('overtime_period'))
+			else:
+				sc2_row.append({
+					'overtime_detail':ov.get('name'),
+					'date':ov.get('date'),
+					'overtime':ov.get('overtime_period')
+				})
+				total_cost_ot_2+=float(ov.get('overtime_cost'))
+				overtime_list2.append(ov.get('overtime_period'))
 
-			if len(sc1_row)>0:
-				as1=frappe.new_doc("Additional Salary")
-				as1.employee=emp.name
-				as1.salary_component='OT 1.5 Amt'
-				as1.amount=total_cost_ot_1
-				as1.total_overtime=calculate_total_overtime(overtime_list1)
-				as1.payroll_date=datetime.date.today()
-				for d in sc1_row:
-					as1.append('overtime_details',d)
-				as1.insert()
+		if len(sc1_row)>0:
+			as1=frappe.new_doc("Additional Salary")
+			as1.employee=emp.name
+			as1.salary_component='OT 1.5 Amt'
+			as1.amount=total_cost_ot_1
+			as1.total_overtime=calculate_total_overtime(overtime_list1)
+			as1.payroll_date=datetime.date.today()
+			for d in sc1_row:
+				as1.append('overtime_details',d)
+			as1.insert()
 
-			if len(sc2_row)>0:
-				as2=frappe.new_doc("Additional Salary")
-				as2.employee=emp.name
-				as2.salary_component='OT 2 Amt'
-				as2.amount=total_cost_ot_2
-				as2.total_overtime=calculate_total_overtime(overtime_list2)
-				as2.payroll_date=datetime.date.today()
-				for d in sc2_row:
-					as2.append('overtime_details',d)
-				as2.insert()
+		if len(sc2_row)>0:
+			as2=frappe.new_doc("Additional Salary")
+			as2.employee=emp.name
+			as2.salary_component='OT 2 Amt'
+			as2.amount=total_cost_ot_2
+			as2.total_overtime=calculate_total_overtime(overtime_list2)
+			as2.payroll_date=datetime.date.today()
+			for d in sc2_row:
+				as2.append('overtime_details',d)
+			as2.insert()
 
 def calculate_total_overtime(ot_list):
 	totalSecs = 0
